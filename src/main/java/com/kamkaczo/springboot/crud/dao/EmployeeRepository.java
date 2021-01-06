9ee74046e1202c8856ab76a9de74bd5565b276f5
Kamil Kaczowka <s21790@pjwstk.edu.pl>
2021-01-06 22:24:32 +0100

    Spring Data REST

--- src/main/java/com/kamkaczo/springboot/crud/dao/EmployeeRepository.java
+++ src/main/java/com/kamkaczo/springboot/crud/dao/EmployeeRepository.java
@@ -3,6 +3,7 @@ package com.kamkaczo.springboot.crud.dao;
 import com.kamkaczo.springboot.crud.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+// @RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 }
